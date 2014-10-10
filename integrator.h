7981a4f69bba932ab8390ea4b1683691bb8c2363
KineOdegardHanssen <kineoha@student.matnat.uio.no>
2014-10-10 18:05:34 +0200

    VerletEvolve is now private.

--- integrator.h
+++ integrator.h
@@ -8,13 +8,14 @@ class Integrator
 private:
     System k_old;
     void evolveSystem1InTimeUsingDerivativesFromSystem2(System &system1, System &system2, double dt);
+    void VerletEvolve(System &system1, double dt);
 
 public:
     Integrator();
     void RK4(System &system, double dt);
     void VerletInitialise(System &system, double dt);
     void Verlet(System &system, double dt);
-    void VerletEvolve(System &system1, double dt);
+
 };
 
 #endif // RK4_H
